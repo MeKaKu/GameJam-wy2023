b0588e739ab51a0e11b23b2c355a0b67bcaf9d4e
OJ <57611861+MeKaKu@users.noreply.github.com>
2023-04-28 15:32:25 +0800

    Merge branch 'main' of https://github.com/MeKaKu/GameJam-wy2023

---
+++ GameJam-wy2023/Assets/Scripts/PathNode.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathNode : MonoBehaviour
+{
+    public PathNode nextNode;
+    void Start()
+    {
+        GameObject[] gb = GameObject.FindGameObjectsWithTag("Path");
+        int a = int.Parse((this.gameObject.name.Remove(0, 8)));
+        if (a < gb.Length)
+        {
+            nextNode = gb[a].GetComponent<PathNode>();
+        }
+    }
+}
